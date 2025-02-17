8cbd65da0fc629736761cb5f447aa8ee6847bdae
David Dung <phamxuandungpy@gmail.com>
2025-02-17 14:18:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 import requests
 
-# Gọi API để lấy dữ liệu
-response = requests.get('http://127.0.0.1:5000/get-data')
-data = response.json()
-
-# Chuyển dữ liệu thành DataFrame
-df = pd.DataFrame(data)
 
 # Hiển thị dữ liệu bằng Streamlit
 st.title('Dữ liệu từ Oracle qua API')
-st.dataframe(df)
