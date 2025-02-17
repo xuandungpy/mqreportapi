6e30b5c9a88cf8d67225bf1a10e0c6a0246a6915
David Dung <phamxuandungpy@gmail.com>
2025-02-17 14:14:50 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify
 import cx_Oracle
-cx_Oracle.init_oracle_client(lib_dir=r"E:\streamlit\setup\instantclient_23_7")
+//cx_Oracle.init_oracle_client(lib_dir=r"E:\streamlit\setup\instantclient_23_7")
 
 app = Flask(__name__)
 
