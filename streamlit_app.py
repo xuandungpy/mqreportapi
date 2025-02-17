3e522162a0f6c56ca0b22f7b0f61016bb8ab51eb
David Dung <phamxuandungpy@gmail.com>
2025-02-17 14:41:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 from flask import Flask, jsonify
 import cx_Oracle
-#cx_Oracle.init_oracle_client(lib_dir=r"E:\streamlit\setup\instantclient_23_7")
 
-app = Flask(__name__)
-
-st.title('Dữ liệu từ Oracle qua API')  
+st.title('MQ Report API')  
